4f027a0c6ff9aa18dba6f4fe20d0978b6032a0bc
ALIAMBO <alibinambo26@gmail.com>
2024-10-07 12:15:32 +0800

    update something

--- MultiDimensionalArray.java
+++ MultiDimensionalArray.java
@@ -3,11 +3,11 @@ public class MultiDimensionalArray {
     
     public static void main(String[] args){
 
-        int     [] var1 = new int[3];
-        double  [] var2 = new double[3];
-        char    [] var3 = new char[3]; 
-        boolean [] var4 = new boolean[3]; 
-        String  [] var5 = new String[3];
+        int     [] var1 = new int     [3];
+        double  [] var2 = new double  [3];
+        char    [] var3 = new char    [3]; 
+        boolean [] var4 = new boolean [3]; 
+        String  [] var5 = new String  [3];
 
         boolean isLoggedIn = true;
         boolean hasPermission = false;
@@ -23,8 +23,8 @@ public class MultiDimensionalArray {
             System.out.println("Exercise 1. ");
             System.out.println("");
             System.out.println("Main array lenght: " + matrix.length);
-
-            for (int i = 0; i < matrix.length; i++) {
+            for (int i = 0; i < matrix.length; i++) 
+            {
                 System.out.println("Sub array lenght: " + matrix[i].length);
                 for(int col =0 ;col < matrix[i].length; col++){
                     System.out.println(matrix[i][col]);
@@ -34,44 +34,46 @@ public class MultiDimensionalArray {
             System.out.println("--------------------------");
             System.out.println("Exercise 2. ");
             System.out.println("");
-            for (int i = 0; i <1; i++) {
-                System.out.println("int: " + var1[i]);
-                System.out.println("double: " + var2[i]);
-                System.out.println("char: " + var3[i]);
-                System.out.println("boolean: " + var4[i]);
-                System.out.println("String: " + var5[i]);
-            }
+                for (int i = 0; i <1; i++) 
+                {
+                    System.out.println("int: " + var1[i]);
+                    System.out.println("double: " + var2[i]);
+                    System.out.println("char: " + var3[i]);
+                    System.out.println("boolean: " + var4[i]);
+                    System.out.println("String: " + var5[i]);
+                }
             
             System.out.println("--------------------------");
             System.out.println("Exercise 3. ");
             System.out.println("");   
-            for (int i = 0; i < numbers.length; i++) {
-                System.out.print(numbers[i]+ ", ");
-             
-            }
+                for (int i = 0; i < numbers.length; i++) 
+                {
+                    System.out.print(numbers[i]+ ", ");          
+                 }
             System.out.println("");
-            System.out.println("Error Array out of boundary test: ");
-                   
-                try {
+            System.out.println("Error Array out of boundary test: ");             
+                try 
+                {
                     for (int i = 0; i < numbers.length; i++) { 
                     System.out.print(numbers[i+4]+ ", ");
                 }
                 } catch (Exception e) {
                     System.out.println("");
                     System.out.println(e);
-                    System.out.println("Please input correct array");
-                    
+                    System.out.println("Please input correct array");                 
                 }
 
             System.out.println("--------------------------");
             System.out.println("Exercise 4. ");
             System.out.println(""); 
             System.out.println("Check if the user is logged in and has permission. ");
-           if (!isLoggedIn || !hasPermission) {
-                System.out.println("The user has logged in.");
-           }else{
-            System.out.println("Wrong credentials. ");
-           }
+                if (!isLoggedIn || !hasPermission) 
+                {
+                    System.out.println("The user has logged in.");
+                }else
+                {
+                    System.out.println("Wrong credentials. ");
+                }
 
         }
 
